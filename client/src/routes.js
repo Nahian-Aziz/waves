1c8a0e37f7e850da21521df6560bd1bc505bb8db
Nahian Bin Aziz <trev@trev.local>
2018-11-12 23:26:09 -0800

    Redux setup

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
-// import axios from 'axios';
 import { Switch, Route } from 'react-router-dom';
 
 import Layout from './hoc/layout';
 import Home from './components/Home';
+import RegisterLogin from './components/Register_login';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Layout>
